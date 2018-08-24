d175e730bc8e678e9de754af9858c0a3dfaa3074
matthew voorhees <voorhees.matthew@gmail.com>
2018-08-23 22:10:25 -0700

    Add initialize for Map class

--- W1D5/exercise_3_map.rb
+++ W1D5/exercise_3_map.rb
@@ -2,14 +2,18 @@
 #
 # Remember, a map stores information in key-value pairs, where the keys are always unique. When implemented with arrays, a map might look something like this: my_map = [[k1, v1], [k2, v2], [k3, v2], ...].
 #
-# Our Map class should have the following instance methods: set(key, value), get(key), delete(key), show. Note that the set method can be used to either create a new key-value pair or update the value for a pre-existing key. It's up to you to check whether a key currently exists!
+# Note that the set method can be used to either create a new key-value pair or update the value for a pre-existing key. It's up to you to check whether a key currently exists!
 #
 # As always, test your code thoroughly to make sure all properties of maps are enforced.
 
 class Map
 
+  def initialize
+    @ivar = []
+  end
+
   def set(key, value)
-    
+
   end
 
   def get(key)
