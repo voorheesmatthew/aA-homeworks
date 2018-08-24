47d22cc23296df5c6b01edd6fb5e0cd5ecec864f
matthew voorhees <voorhees.matthew@gmail.com>
2018-08-23 21:58:32 -0700

    Complete Exercise 1

--- W1D5/exercise_1_stack.rb
+++ W1D5/exercise_1_stack.rb
@@ -1,17 +1,21 @@
 class Stack
-    def initialize
-      # create ivar to store stack here!
-    end
-
-    def push(el)
-      # adds an element to the stack
-    end
-
-    def pop
-      # removes one element from the stack
-    end
-
-    def peek
-      # returns, but doesn't remove, the top element in the stack
-    end
+  def initialize
+    # create ivar to store stack here!
+    @ivar = []
   end
+
+  def push(el)
+    # adds an element to the stack
+    @ivar.push(el)
+  end
+
+  def pop
+    # removes one element from the stack
+    @ivar.pop
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+    @ivar.last
+  end
+end
