900acd0a524198b68533c1e2bbaa151548ec6949
matthew voorhees <voorhees.matthew@gmail.com>
2018-08-30 20:49:34 -0700

    Write first LRUCache

--- W2D5/lru_exercise.rb
+++ W2D5/lru_exercise.rb
@@ -1,17 +1,32 @@
 class LRUCache
-    def initialize
+    def initialize(cache_size)
+      @cache = Array.new
+      @max_cache_size = cache_size
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.count
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      if @cache.include?(el)
+        @cache.delete(el)
+        @cache.push(el)
+      else
+        if @cache.count == @max_cache_size
+          @cache.shift
+          @cache.push(el)
+        else
+          @cache.push(el)
+        end
+      end
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      @cache
     end
 
     private
