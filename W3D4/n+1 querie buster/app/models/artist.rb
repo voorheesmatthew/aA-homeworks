5ce02b239148280aa1ac16abe9f5553910cc2283
matthew voorhees <voorhees.matthew@gmail.com>
2018-09-06 08:43:00 -0700

    Final attempt

--- W3D4/n+1 querie buster/app/models/artist.rb
+++ W3D4/n+1 querie buster/app/models/artist.rb
@@ -15,6 +15,25 @@ class Artist < ApplicationRecord
   end
 
   def better_tracks_query
-    # TODO: your code here
+    # albums = self.albums.select('*, COUNT(*) AS albums_count').join(:tracks).group('albums.id')
+    #
+    # tracks_count = {}
+    # albums.each do |album|
+    #   tracks_count[album.title] = album.albums_count
+    # end
+    #
+    # tracks_count
+    albums = self
+      .albums
+      .select('albums.*, COUNT(*) AS tracks_count')
+      .joins(:tracks)
+      .group('albums.id')
+
+    album_counts = {}
+    albums.each do |album|
+      album_counts[album.title] = album.tracks_count
+    end
+
+    album_counts
   end
 end
