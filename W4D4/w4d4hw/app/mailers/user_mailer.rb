eb340ebbdc77decb228cb29b7fcabcc44a358eb5
matthew voorhees <voorhees.matthew@gmail.com>
2018-09-13 09:42:26 -0700

    Fix phase III

--- W4D4/w4d4hw/app/mailers/user_mailer.rb
+++ W4D4/w4d4hw/app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: 'everybody@appacademy.io'
+
+   def welcome_email(user)
+    @user = user
+    @url  = 'https://mattvoorhees.com'
+    mail(to: user.username, subject: 'Welcome to 99 cats, bro!')
+   end
+
 end
