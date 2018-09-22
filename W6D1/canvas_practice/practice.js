b7a8a3773a6b46d808c70e1c8cf7b7c535552438
Matt Voorhees <voorhees.matthew@gmail.com>
2018-09-22 14:09:23 -0700

    Complete W6D1 HW

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById("mycanvas");
+  canvas.width = 500;
+  canvas.height = 500;
 
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(75, 75, 50, 0, 2 * Math.PI);
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.fillStyle = "green";
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.fillStyle = "blue";
+  ctx.fill();
 });
